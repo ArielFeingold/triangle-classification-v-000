d1b884345bb9a1cd9184d221c9ffe811e744f1d8
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 11:56:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,15 +6,28 @@ class Triangle
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
+     @sides_array = [@side_1, @side_2, @side_3]
   end
 
-  def kind
-    if @side_one == @side_two && @side_two == @side_three
-      :equilateral
-    end
+  def valid?
+     sorted_sides = @sides_array.sort
+     valid_lengths = @sides_array.all? { |side| side >= 0 }
+     valid_lengths && (sorted_sides[0] + sorted_sides[1] > sorted_sides[2])
+   end
+
+   def kind
+     if valid?
+       case @sides_array.uniq.size
+       when 1 then :equilateral
+       when 2 then :isosceles
+       else        :scalene
+       end
+     else
+       raise TriangleError
+     end
+   end
+
+  class TriangelEerror < StandardError
+
   end
-end
-
-class TriangelEerror < StandardError
-
 end
