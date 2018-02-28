7b2e76326d62f04dbd88581172b6f9c5bd3bdf84
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 11:57:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,33 +1,31 @@
 class Triangle
+  attr_accessor :side_1, :side_2, :side_3
 
-  attr_accessor :side_one, :side_two, :side_three
-
-  def initialize (side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three = side_three
-     @sides_array = [@side_1, @side_2, @side_3]
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @sides_array = [@side_1, @side_2, @side_3]
   end
 
   def valid?
-     sorted_sides = @sides_array.sort
-     valid_lengths = @sides_array.all? { |side| side >= 0 }
-     valid_lengths && (sorted_sides[0] + sorted_sides[1] > sorted_sides[2])
-   end
-
-   def kind
-     if valid?
-       case @sides_array.uniq.size
-       when 1 then :equilateral
-       when 2 then :isosceles
-       else        :scalene
-       end
-     else
-       raise TriangleError
-     end
-   end
-
-  class TriangelEerror < StandardError
-
+    sorted_sides = @sides_array.sort
+    valid_lengths = @sides_array.all? { |side| side >= 0 }
+    valid_lengths && (sorted_sides[0] + sorted_sides[1] > sorted_sides[2])
   end
+
+  def kind
+    if valid?
+      case @sides_array.uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
